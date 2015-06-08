1483333d35450586fc70054b055dc46e81dd2184
eisd <unknown>
2015-06-08 19:53:42 +0000

    nasty hacks

--- main.cpp
+++ main.cpp
@@ -5,43 +5,58 @@
 
 #include "WiiChuk_compat.hpp"
 
+//#include "USBSerial.h"
+
+#include "RFM69.h"
+#define GATEWAY_ID    2
+#define NODE_ID       1
+#define NETWORKID     100
+
+// Uncomment only one of the following three to match radio frequency
+//#define FREQUENCY     RF69_433MHZ    
+#define FREQUENCY     RF69_868MHZ
+//#define FREQUENCY     RF69_915MHZ
+
 #ifndef USBSerial
-Serial pc(USBTX, USBRX);
+    Serial pc(USBTX, USBRX);
 #endif
-
-#ifdef TARGET_KL25Z
-WiiChuck nun(PTE0, PTE1, pc);
-PwmOut r(LED_RED);
-PwmOut g(LED_GREEN);
-PwmOut b(LED_BLUE);
-#else
-#include "USBSerial.h"
-WiiChuck nun(p9, p10, pc);
-#endif
-
 int main() {
-#ifdef TARGET_KL25Z
-    r = 1.0f;
-    g = 1.0f;
-    b = 1.0f;
-#endif
-
 #ifndef USBSerial
     pc.baud(115200);
 #endif
 
+#ifdef TARGET_KL25Z
+    PwmOut r(LED_RED);
+    r = 1.0f;
+    PwmOut g(LED_GREEN);
+    g = 1.0f;
+    PwmOut b(LED_BLUE);
+    b = 1.0f;
+    //WiiChuck nun(PTE0, PTE1, pc);
+    RFM69 radio(PTD2, PTD3, PTC5, PTD0, PTA13);
+#else
+    WiiChuck nun(p9, p10, pc);
+#endif
+
+    pc.printf("-- %d --\r\n", radio.initialize(FREQUENCY, NODE_ID, NETWORKID));
+    radio.encrypt("0123456789012345");
+    //radio.promiscuous(false);
+    //radio.setHighPower(true);
+    radio.setPowerLevel(31);
+    //radio.rcCalibration();
+    //radio.readAllRegs();
+    pc.printf("temp %d\r\n", radio.readTemperature(-1));
     pc.printf("wii test begin                                             \r\n");
 
-    int joyX = 0;int joyY = 0;
-    int accX = 0;int accY = 0;int accZ = 0;
-    int buttonC = 0;int buttonZ = 0;
+
+    nunchuk n;
      
     while(1) {
-        bool read = nun.Read(&joyX,&joyY,&accX,&accY,&accZ,&buttonC,&buttonZ);
+        bool read = true;//nun.Read(&n.X, &n.Y, &n.aX, &n.aY, &n.aZ, &n.C, &n.Z);
         if(read)
         {
-            float x = joyX - 128, y = joyY - 128;
-            float R = x*x + y*y, p = atan2(y, x) * 4 / M_PI - 0.5;
+            float x = n.X - 128, y = n.Y - 128;
+            float R = 1000, p = 90.0f;//x*x + y*y, p = atan2(y, x) * 4 / M_PI - 0.5;
             int c = 0;
             const char *d = "RF";
             if (p > -4) {c = 0; d = "XR";}
@@ -53,9 +68,12 @@ int main() {
             if (p >  2) {c = 6; d = "XF";}
             if (p >  3) {c = 7; d = "RF";}
             
-            pc.printf("x%3d y%3d c%1d z%1d --", joyX, joyY, buttonC, buttonZ);
-            pc.printf("x%d y%d z%d -- %.3f %s                   \r\n", accX, accY, accZ, R, d);
+            //pc.printf("x%3d y%3d c%1d z%1d --", n.X, n.Y, n.C, n.Z);
+            //pc.printf("x%d y%d z%d -- %.3f %s                   \r\n", n.aX, n.aY, n.aZ, R, d);
 
+            radio.send(GATEWAY_ID, (const void*)"A", 1, false);
+            //radio.send(GATEWAY_ID, (const void*)&n, sizeof(nunchuk), false);
+            pc.printf("A");
 #ifdef TARGET_KL25Z
             if (R < 100) {
                 r = 1.0f;
@@ -84,6 +102,6 @@ int main() {
             pc.printf("Error\r\n");
             wait(1);
         }
-        wait(0.01);
+        wait(0.1);
     }
 }
